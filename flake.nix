95ebfe858b43253d7c98bf55281044b35ea22018
VitalyR <vr@vitalyr.com>
2024-11-22 00:22:52 +0800

    Make swapping ctrl and capslock via xremap work

--- flake.nix
+++ flake.nix
@@ -55,15 +55,19 @@
               name = "Global";
               remap = { "CapsLock" = "CTRL_L"; }; # globally remap CapsLock to Ctrl
             }
+	    {
+              name = "Example CTRL_L > CapsLock";
+              remap = { "CTRL_L" = "CapsLock"; };
+            }
           ];
 
           # Keymap for key combo rebinds
           services.xremap.config.keymap = [
-            {
-              name = "Example ctrl-u > pageup rebind";
-              remap = { "CapsLock" = "CTRL_L"; };
+            #{
+            #  name = "Example ctrl-u > pageup rebind";
+            #  remap = { "CapsLock" = "CTRL_L"; };
               # NOTE: no application-specific remaps work without features (see configuration)
-            }
+            #}
           ];
         }
         ];
